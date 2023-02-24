c302f4b8f1d2c2d8d5d8a13daba9363a91cc348b
m-molinari <localhost@localdomain>
2023-02-24 22:25:04 +0100

    updare sync script

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-if  [ -e /etc/debian_version ] ; then
-	DISTRO=debian
-elif [ -e /etc/arch-release ] ; then
-	DISTRO=archlinux
-elif [ -e /etc/gentoo-release  ] ; then
-        DISTRO=gentoo
-else
-	echo -e "\033[0;31m distro not found \033[0m"  
-	exit 1
-fi
-
 git pull
 
 if [ "$2" == "dry" ] ; then
-	rsync -avz --dry-run -b --backup-dir=old_dotfiles  --include .[^.]* $DISTRO/ --exclude .git --exclude README.md  $HOME/
+	rsync -avz --dry-run -b --backup-dir=old_dotfiles  --include .[^.]*  --exclude .git --exclude README.md  $HOME/
 else
-	rsync -avz -b --backup-dir=old_dotfiles  --include .[^.]* $DISTRO/ --exclude .git --exclude README.md  $HOME/
+	rsync -avz -b --backup-dir=old_dotfiles  --include .[^.]* --exclude .git --exclude README.md  $HOME/
 fi
 
 echo 
