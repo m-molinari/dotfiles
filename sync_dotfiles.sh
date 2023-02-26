e6a6bd51cd4d864bbb91bef1624012d49af769f8
m-molinari <localhost@localdomain>
2023-02-26 12:40:10 +0100

    removed dry run

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -2,11 +2,7 @@
 
 git pull
 
-if [ "$2" == "dry" ] ; then
-	rsync -avz --dry-run -b --backup-dir=old_dotfiles  --include .[^.]*  --exclude .git --exclude README.md  $HOME/
-else
-	rsync -avz -b --backup-dir=old_dotfiles  --include .[^.]* --exclude .git --exclude README.md  $HOME/
-fi
+rsync -avz -b --backup-dir=old_dotfiles  --include .[^.]* --exclude .git --exclude README.md  $HOME/
 
 echo 
 echo -e  "\033[0;32m sync $DISTRO config \033[0m"
